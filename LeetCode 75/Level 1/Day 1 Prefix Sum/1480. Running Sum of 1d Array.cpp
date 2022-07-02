25b2678f5f7c21309cb00a17cf04f1fb218d87b7
Joton Sutradhar <jotonsutradharjoy@gmail.com>
2022-07-02 22:50:31 +0600

    Create 1480. Running Sum of 1d Array.cpp

---
+++ LeetCode 75/Level 1/Day 1 Prefix Sum/1480. Running Sum of 1d Array.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> runningSum(vector<int>& nums) {
+
+    vector <int> result;
+    auto sum = 0;
+    for(int i=0; i < nums.size(); i++)
+    {
+        // implementing result size
+        result.push_back(i);
+
+        // calculating the sum value
+        sum = sum + nums[i];
+
+        // assigning the value into the result key
+        result[i] = sum;
+
+    }
+    return result;
+}
+int main ()
+{
+    vector <int> nums = {1,2,3,4};
+    // calling the
+    runningSum(nums);
+
+    return 0;
+}
