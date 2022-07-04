781f2b7e5465d6c1b8a282c34cd73f4cf2f13c6c
Joton Sutradhar <jotonsutradharjoy@gmail.com>
2022-07-04 12:39:19 +0600

    update

---
+++ LeetCode 75/Level 1/Day 2 String/205. Isomorphic Strings.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isIsomorphic(string s, string t) {
+    vector<int> map1(256, -1);
+        vector<int> map2(256, -1);
+        
+        int length = s.size();
+        
+        for (int i = 0; i<length; i++){
+            if (map1[s[i]] != map2[t[i]]) {
+                return false;
+            }
+            // s[i] and t[i] should map to a same digits.
+            map1[s[i]] = map2[t[i]] = i;
+        }
+        return true;
+    
+}
+int main ()
+{
+    string s = "stenographics", t = "logarithmsxox";
+    cout<<isIsomorphic(s,t);
+    return 0;
+}
---
+++ LeetCode 75/Level 1/Day 2 String/392. Is Subsequence.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+bool isSubsequence(string s, string t) {
+    int count = 0;
+    if(s.length()> t.length())
+        return false;
+    for (auto i = 0; i < t.length(); i++)
+    {
+        if (s[count] == t[i])
+        {
+            count++;
+        }
+        else{
+            continue;
+        }
+        
+    }
+    if (count == s.length())
+    {        
+        return true;
+    }else{
+        return false;
+    }
+    
+    
+}
+int main ()
+{
+    string s = "abc", t = "sahbgddc";
+    cout<<isSubsequence(s,t);
+    return 0;
+}
